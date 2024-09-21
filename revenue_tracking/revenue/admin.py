7a4148193f534c941094c052605c6ac2dc8600c3
S M Shahinul Islam <imshahin1988@gmail.com>
2024-09-21 04:11:10 -0400

    Enable Django’s built-in admin to allow the admin users to input revenue data

--- revenue_tracking/revenue/admin.py
+++ revenue_tracking/revenue/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Project, Task, RevenueLog
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('project', 'name')
+    list_filter = ('project',)
+
+
+@admin.register(RevenueLog)
+class RevenueLogAdmin(admin.ModelAdmin):
+    list_display = ('task', 'date', 'revenue')
+    list_filter = ('task', 'date')
+    search_fields = ('task__name',)
