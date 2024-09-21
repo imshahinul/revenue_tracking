b100afef464fbb822a1171068473a98151d0f978
S M Shahinul Islam <imshahin1988@gmail.com>
2024-09-21 04:43:58 -0400

    Create API views functions

--- revenue_tracking/revenue/views.py
+++ revenue_tracking/revenue/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Task, RevenueLog
+from .serializers import ProjectSerializer, TaskSerializer, RevenueLogSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class RevenueLogViewSet(viewsets.ModelViewSet):
+    queryset = RevenueLog.objects.all()
+    serializer_class = RevenueLogSerializer
