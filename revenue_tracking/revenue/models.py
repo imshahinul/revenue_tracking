8b96cd903aaad3c65778b340fa0edf696a8ebb68
S M Shahinul Islam <imshahin1988@gmail.com>
2024-09-21 03:57:42 -0400

    Add new model Task with three fields name, description and project as foriegn key related to model Project

--- revenue_tracking/revenue/models.py
+++ revenue_tracking/revenue/models.py
@@ -6,3 +6,15 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('project', 'name')
+
+    def __str__(self):
+        return f"{self.project.name} - {self.name}"
