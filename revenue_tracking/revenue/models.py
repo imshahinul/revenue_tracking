0ad9add5cd7ca09b1002af793424df22b2764066
S M Shahinul Islam <imshahin1988@gmail.com>
2024-09-21 04:00:07 -0400

    Add new model RevenueLog with three fields date, revenue and task as foriegn key related to model Task

--- revenue_tracking/revenue/models.py
+++ revenue_tracking/revenue/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Project(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -18,3 +20,15 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.project.name} - {self.name}"
+
+
+class RevenueLog(models.Model):
+    task = models.ForeignKey(Task, related_name='revenue_logs', on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    revenue = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ('task', 'date')
+
+    def __str__(self):
+        return f"{self.task.name} - {self.date} - ${self.revenue}"
