7f116ce256edb228269d4f7f73450035b695abf3
S M Shahinul Islam <imshahin1988@gmail.com>
2024-09-22 04:02:11 -0400

    add oauth and changed related settings

--- revenue_tracking/revenue_tracking/settings.py
+++ revenue_tracking/revenue_tracking/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'revenue',
     'rest_framework',
+    'oauth2_provider',
+    'corsheaders',  # Enable CORS if external apps will access the API
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# DRF Configuration with OAuth2
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 ROOT_URLCONF = 'revenue_tracking.urls'
 
